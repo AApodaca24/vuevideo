872c05921a72413f6d70187f4d274ad668323979
Anthony Apodaca <anthony.r.apodaca@gmail.com>
2020-09-25 11:15:42 -0600

    initial commit

---
+++ src/components/video.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <vue-core-video-player
+      ref="player"
+      :src="currentVideo"
+      @ended="playNext"
+      @loadeddata="play"
+    ></vue-core-video-player>
+  </div>
+</template>
+
+<script>
+const video = require('../assets/demo.mp4');
+const video2 = require('../assets/demo2.mp4');
+const video3 = require('../assets/demo3.mp4');
+
+const videoArr = [video, video2, video3];
+
+export default {
+  name: 'vueVideo',
+  data: () => ({
+    demoVideos: videoArr,
+    currentVideo: videoArr[0],
+  }),
+  methods: {
+    play() {
+      this.$refs.player.play();
+    },
+    playNext() {
+      let index = this.demoVideos.findIndex(e => this.currentVideo === e);
+      if (index === this.demoVideos.length - 1) {
+        const [nextVideo] = this.demoVideos;
+        this.currentVideo = nextVideo;
+        console.log(this.currentVideo);
+      } else {
+        index += 1;
+        const nextVideo = this.demoVideos.find(
+          e => e === this.demoVideos[index],
+        );
+        this.currentVideo = nextVideo;
+      }
+    },
+  },
+  mounted() {
+    console.log(this.$refs.player);
+  },
+};
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
+import VueCoreVideoPlayer from 'vue-core-video-player';
 import App from './App.vue';
 import router from './router';
 
+Vue.use(VueCoreVideoPlayer);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: (h) => h(App),
+  render: h => h(App),
 }).$mount('#app');
